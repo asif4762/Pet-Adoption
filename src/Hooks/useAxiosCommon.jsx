1a236dc92decc7f21b6c93e6036b78c296858d2c
asif4762 <zasif855@gmail.com>
2024-07-30 16:07:53 +0600

    some update done

--- src/Hooks/useAxiosCommon.jsx
+++ src/Hooks/useAxiosCommon.jsx
@@ -1,13 +1,7 @@
 import axios from "axios";
 
-// Correctly access the environment variable
-const baseURL = import.meta.env.VITE_API_URL;
-
-// Log the baseURL to check if it is correctly retrieved
-console.log(baseURL);
-
 const axiosCommon = axios.create({
-    baseURL: baseURL, // Use baseURL directly without curly braces
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 const useAxiosCommon = () => {
